7cd17e8c92111a18751f0fcfd2fcfb7ac73ed3a1
Slava Kravchenko <cordawyn@neptune.(none)>
2012-07-24 00:52:26 +0300

    renamed test classes to match to docs (README)

--- spec/lib/active_redlander/resource_spec.rb
+++ spec/lib/active_redlander/resource_spec.rb
@@ -2,33 +2,33 @@ require "spec_helper"
 
 describe ActiveRedlander::Resource do
   before :all do
-    module Person
+    module Human
       include ActiveRedlander::Resource
-      type "http://schema.org/Person"
+      type "http://schema.org/Human"
     end
 
-    class TestResource
-      extend Person
+    class Person
+      extend Human
     end
   end
 
-  subject { Person }
+  subject { Human }
 
   it { should respond_to :type }
 
   describe "extended class" do
-    subject { TestResource }
+    subject { Person }
 
     it { should_not respond_to :type }
 
     it { should respond_to :types }
 
     describe "types" do
-      subject { TestResource.types }
+      subject { Person.types }
 
       it { should be_a Set }
 
-      it { should include URI("http://schema.org/Person") }
+      it { should include URI("http://schema.org/Human") }
     end
   end
 end
